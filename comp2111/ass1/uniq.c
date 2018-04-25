5915f72fd75b1b1389708eed0af33b3b654dc9a0
FionaCLin <clin4614@uni.sydney.edu.au>
2018-04-25 23:22:56 +1000

    Commit the final report of assignment1

--- comp2111/ass1/uniq.c
+++ comp2111/ass1/uniq.c
@@ -7,15 +7,26 @@ unsigned int cmp(char *a, char *b) ;
 
 unsigned int uniq(unsigned int n, char *a[], char *b[]) {
     int k = 0;
+    /*Here opted for the more traditional for loop idiom instead of a while loop.*/
     for(int i = 0; i < n; i ++) {
+        /*first if i = 0 branch to string copy the first element to string array b*/
         if (i == 0) {
+            /*copy the first element into b[0]*/
             b[i] = malloc(strlen(a[i]));
             strcpy(b[i], a[i]);
         } else {
+        /*first if else branch to string compare the first */
             int h = 0;
+            /*In c implementation, checking individual characters 
+            in both string arrays to preform the string compare. 
+            Here opted for a and b pointer nullity checking, and 
+             characters comparison as well as h increment */
             for (; a != NULL && a[i][h] != '\0' && b != NULL && b[k][h] == a[i][h]; h++);
+            /*Check if exist h make a[i][h] and b[k][h] different */
             if (a[i][h] != b[k][h]) {
+            /*increment k and alloction memory in c implementation*/
                 b[++k] = malloc(strlen(a[i]));
+            /*assign a[i] to b[k]*/
                 strcpy(b[k],a[i]);
             }
         }
