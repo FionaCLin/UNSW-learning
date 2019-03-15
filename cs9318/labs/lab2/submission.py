f71153b6b0a1ee4b397710bf0ae5140d41c890dd
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-03-15 22:50:57 +1100

    Half working solution

--- cs9318/labs/lab2/submission.py
+++ cs9318/labs/lab2/submission.py
@@ -7,4 +7,10 @@ import numpy as np
 ################# Question 1 #################
 
 def v_opt_dp(x, num_bins):# do not change the heading of the function
-    pass # **replace** this line with your code
+    res = pd.qcut(np.array(x), num_bins,retbins=True,)
+    left=sorted(x[:-1])
+    right=sorted(x[1:])
+    bins = pd.IntervalIndex.from_arrays(left,right)
+    print(bins)
+
+    return res
